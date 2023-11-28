85e54b79fdf407eb9a50ced5401a17bf913d0b91
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2023-11-28 12:47:38 +0530

    Update stateContext.jsx

--- src/context/stateContext.jsx
+++ src/context/stateContext.jsx
@@ -1,18 +1,16 @@
-import {
-  createContext,
-  useEffect,
-  useRef,
-  useState,
-  useMemo,
-  useCallback,
-} from "react";
+// Importing necessary dependencies and modules from external libraries
+import React, { createContext, useEffect, useRef, useState, useMemo, useCallback } from "react";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
 import collect from "collect.js";
 
+// Create a React context to manage the state
 const StateContext = createContext();
 
+// Defining a provider component that will wrap the application and provide state to its children
 const StateProvider = ({ children }) => {
+  
+  // State variables to manage various form and invoice-related data
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
@@ -34,20 +32,25 @@ const StateProvider = ({ children }) => {
   const [total, setTotal] = useState(0);
   const [isEditing, setIsEditing] = useState(false);
   const [showModal, setShowModal] = useState(false);
+
+  // Ref for referencing a component in the DOM
   const componentRef = useRef();
 
+   // Function to handle printing
   const handlePrint = useCallback(() => {
     window.print();
   }, []);
 
-  // Submit form function
+  // Form submission function
   const handleSubmit = useCallback(
     (e) => {
       e.preventDefault();
 
+      // Validation check for form inputs
       if (!description || !quantity || !price) {
         toast.error("Please fill in all inputs");
       } else {
+        // Creating a new item with a unique ID using uuid
         const newItems = {
           id: uuidv4(),
           description,
@@ -55,31 +58,31 @@ const StateProvider = ({ children }) => {
           price,
           amount,
         };
+
+        // Clearing input fields and updating the list of items
         setDescription("");
         setQuantity("");
         setPrice("");
         setAmount("");
         setList([...list, newItems]);
         setIsEditing(false);
-        console.log(list);
+        //console.log(list);
       }
     },
     [amount, description, list, price, quantity]
   );
 
-  // Calculate items amount function
+  // Function to calculate the amount based on quantity and price
   useEffect(() => {
     const calculateAmount = () => {
       setAmount(quantity * price);
     };
-
     calculateAmount(amount);
   }, [amount, price, quantity, setAmount]);
 
-  // Use collect.js to calculate the total amount of items in the table.
+  // Function to calculate the total amount of items in the table using collect.js
   const calculateTotal = () => {
     const allItems = list.map((item) => item.amount);
-
     setTotal(collect(allItems).sum());
   };
 
@@ -87,7 +90,7 @@ const StateProvider = ({ children }) => {
     calculateTotal();
   });
 
-  // Edit function
+  // Function to edit a row in the table
   const editRow = useCallback(
     (id) => {
       const editingRow = list.find((row) => row.id === id);
@@ -100,15 +103,14 @@ const StateProvider = ({ children }) => {
     [list]
   );
 
-  // Delete function
+ // Function to delete a row in the table
   const deleteRow = useCallback(
     (id) => {
       setList(list.filter((row) => row.id !== id));
       setShowModal(false);
-    },
-    [list]
-  );
+    },[list]);
 
+  // Creating a context object with all the state variables and functions
   const context = useMemo(
     () => ({
       name,
@@ -210,9 +212,11 @@ const StateProvider = ({ children }) => {
     ]
   );
 
+  // Providing the context to the components in the tree
   return (
     <StateContext.Provider value={context}>{children}</StateContext.Provider>
   );
 };
 
+// Exporting the context and provider for use in other parts of the application
 export { StateContext, StateProvider };
